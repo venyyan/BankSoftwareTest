6ffc04b1630f103e331ee0bdac1b0fe84727bd4d
Venelina Andreeva <venian2003@abv.bg>
2022-12-27 13:46:06 +0200

    added some things

--- BankSoftware1/BankSoftware1.cpp
+++ BankSoftware1/BankSoftware1.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
+	fstream myFile1;
+	myFile1.open("users.txt", fstream::out | fstream::app);
+	string input;
+	getline(cin, input);
+	myFile1 << input << "\n";
+	myFile1.close();
+	fstream myFile2;
+	myFile2.open("users.txt", fstream::in);
+	string buffer;
 
+	while (getline(myFile2, buffer)) {
+		cout << buffer << "\n";
+	}
+	myFile2.close();
 }
